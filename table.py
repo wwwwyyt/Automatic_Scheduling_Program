a9d22667a33f97f800e90ec5c13d8b03a3f259d6
wpy <wwwwyyt@126.com>
2024-06-20 14:28:48 +0800

    GUI功能初步实现 TODO：目前从文本框输入到输出之间需要借助文件data.txt，尝试简化

---
+++ table.py
@@ -0,0 +1,35 @@
+class Table:
+    # 初始化方法，默认从data.txt初始化
+    def __init__(self, data):
+        with open(infile_name, 'r', encoding='utf-8') as infile:
+            self.infile_lines = infile.readlines()
+
+        self.line_count = len(self.infile_lines)
+        self.col_count = len(self.infile_lines[0].split())
+
+        # 检查文件格式并将读取内容按空白字符分割，最终输出表格table
+        self.table = []
+        try:
+            self.__check_format()
+            for line in self.infile_lines:
+                self.table.append(line.split())
+            self.table = tuple(self.table)
+        except ValueError as e:
+            print(e)
+            x = input('程序错误终止，按任意键退出...')
+
+    # 检查格式
+    def __check_format(self):
+        standard_col_count = self.col_count
+        if standard_col_count <= 2:
+            raise ValueError("表格文件列数过少")
+        is_first_line = True
+        for row in self.infile_lines:
+            if is_first_line:
+                words = row.split()
+                standard_col_count = len(words)
+                is_first_line = False
+            else:
+                words = row.split()
+                if len(words) != standard_col_count:
+                    raise ValueError("表格文件格式错误")
